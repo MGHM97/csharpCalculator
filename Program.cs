ef5fad134ce966045e34e2fabaec53b05e6c641d
MGHM97 <mateusoliveira469@gmail.com>
2025-01-07 14:27:50 -0400

    sum

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csharpCalculator {
+  class Program {
+    static void Main (string[] args) {
+      Sum();
+    }
+
+    static void Sum () {
+      Console.Clear();
+      Console.WriteLine("First value: ");
+      float firstValue = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Second value: ");
+      float secondValue = float.Parse(Console.ReadLine());
+
+      Console.WriteLine($"Sum result: {firstValue + secondValue}");
+    }
+  }
+}
